554f3d8b0aeca62725685ef0a61bbd5276b14c3b
ranjit95Ashura <ranjitsuryawanshi952@gmail.com>
2025-04-09 17:42:03 +0530

    added bearish - neveen icons

---
+++ src/app/img/index.ts
@@ -0,0 +1,24 @@
+import SideBarIcon from "./sidebar-left.svg";
+import SearchIcon from "./search.svg";
+import CommondSqaureIcon from "./command-square.svg";
+import DashBoardHome from "./home.svg";
+import SettingsIcon from "./setting.svg";
+import DocumentTextIcon from "./document-text.svg";
+import TimeLineIcon from "./calendar-2.svg";
+import ReportIcon from "./chart-square.svg";
+import ArrowUpRight from "./arrow-up-right.svg";
+import ArrowDownRight from "./arrow-down-right.svg";
+
+
+export {
+    SideBarIcon,
+    SearchIcon,
+    CommondSqaureIcon,
+    DashBoardHome,
+    SettingsIcon,
+    DocumentTextIcon,
+    TimeLineIcon,
+    ReportIcon,
+    ArrowUpRight,
+    ArrowDownRight
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './app/components';
+export * from './app/img';
 export { default as Layout } from './app/layout';
 export { default as Page } from './app/page';
