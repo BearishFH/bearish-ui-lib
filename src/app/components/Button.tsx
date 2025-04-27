24d4f9b099fc823169ec76490b9f85e5024c5b27
ranjit95Ashura <ranjitsuryawanshi952@gmail.com>
2025-04-27 21:07:08 +0530

    remove word

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -14,7 +14,7 @@ export const Button = ({ children, ...props }: Props) => {
       className="!bg-gradient-to-r !from-pink-500 !via-red-500 !to-yellow-500 !text-white !px-8 !py-4 !text-lg !rounded-full !shadow-2xl !font-extrabold !transition-transform !duration-300 !ease-in-out hover:!scale-110 active:!scale-95 focus:!ring-4 focus:!ring-yellow-300"
       {...props}
     >
-      {children}what 
+      {children} 
     </MuiButton>
   );
 };
